f8de654d9a404371efd580c0855d7919f41aa9d6
asqwklop12 <asqwklop12@gmail.com>
2021-04-13 21:08:04 +0900

    init

---
+++ src/main/java/whiteship/toyproject/livestduy/LivestduyApplication.java
@@ -0,0 +1,13 @@
+package whiteship.toyproject.livestduy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivestduyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LivestduyApplication.class, args);
+  }
+
+}
