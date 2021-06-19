d0f298661951e3d2ec35ad6b16b746d486f8ab08
sophysophysophysophy <61459@naver.com>
2021-06-20 08:09:23 +0900

    [BUILD] divide env setting in application.yml

--- src/test/java/whiteship/toyproject/livestduy/LivestduyApplicationTests.java
+++ src/test/java/whiteship/toyproject/livestduy/LivestduyApplicationTests.java
@@ -2,8 +2,10 @@ package whiteship.toyproject.livestduy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LivestduyApplicationTests {
 
   @Test
