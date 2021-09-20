f88173940ddc8f85023b844267ccb39021c9529d
sophysophysophysophy <61459@naver.com>
2021-09-20 11:14:37 +0900

    2021.09.20 pair programming commit

--- src/main/java/whiteship/toyproject/livestudy/common/repository/UserInfoRepository.java
+++ src/main/java/whiteship/toyproject/livestudy/common/repository/UserInfoRepository.java
@@ -3,6 +3,6 @@ package whiteship.toyproject.livestudy.common.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import whiteship.toyproject.livestudy.common.model.UserInfo;
 
-public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
+public interface UserInfoRepository extends JpaRepository<UserInfo, String> {
 
 }
